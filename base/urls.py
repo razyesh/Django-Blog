912fd176e5f6f7085a9c537f6510fd640beecb28
razyesh <pudasainirajesh504@gmail.com>
2019-07-20 22:00:34 +0545

    changes commited

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name="base"),
     path('<slug:slug>', views.PostDetail.as_view(), name="post-detail"),
     path('category/<int:id>', views.PostListCategories.as_view(), name="category-posts"),
-    path('subscribe', views.subscribe, name="subscribe-form"),
+    path('blog/subscribe', views.subscribe, name="subscribe-form"),
     path('category/subscribe', views.subscribe, name="subscribe-form"),
     path('video', views.VideoListView.as_view(), name="video")
 ]
