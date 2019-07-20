eca1689cac1182d227861ff122569f00a4997e87
razyesh <pudasainirajesh504@gmail.com>
2019-07-20 21:36:21 +0545

    Debug False

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&2-78x%*m=blfk8#s9#8j&t@b*#n1idoa2pow)l$x-hu36da)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://razyeshblog.herokuapp.com']
 
 
 # Application definition
