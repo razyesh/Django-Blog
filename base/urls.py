c1a2eecb4f7129dc5f4c2cb28f5bcef3dff228cb
razyesh <pudasainirajesh504@gmail.com>
2019-08-31 14:18:46 +0545

    2019 update

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name="base"),
     path('<slug:slug>', views.PostDetail.as_view(), name="post-detail"),
-    path('category/<int:id>', views.PostListCategories.as_view(), name="category-posts"),
+    path('category/<slug:slug>', views.PostListCategories.as_view(),
+         name="category-posts"),
     path('blog/subscribe', views.subscribe, name="subscribe-form"),
-    path('category/subscribe', views.subscribe, name="subscribe-form"),
-    path('video', views.VideoListView.as_view(), name="video")
+    path('posts/video', views.VideoListView.as_view(), name="video")
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Category, Subscribe, Videos
 from django.views.generic import ListView, DetailView, CreateView
 from .forms import SubscribeForm
@@ -11,10 +11,9 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories']= Category.objects.all()
+        context['categories'] = Category.objects.all()
         context['category_nepal'] = Post.objects.filter(category='1')
         context['category_automobile'] = Post.objects.filter(category='2')
         context['category_mobbiles'] = Post.objects.filter(category='3')
@@ -22,54 +21,59 @@ class PostListView(ListView):
         context['category_gadget_review'] = Post.objects.filter(category='5')
         context['category_jobs'] = Post.objects.filter(category='6')
         context['videos'] = Videos.objects.all()
-        return context 
+        return context
+
 
 class PostListCategories(ListView):
     model = Post
-    template_name = "homepage/index.html"
-    context_object_name = 'posts'
+    template_name = "homepage/category.html"
+    context_object_name = 'category_posts'
     paginate_by = 5
 
     def get_queryset(self):
-        posts = Post.objects.all()
-        posts = posts.filter(category = self.kwargs['id'])
-        return posts
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories']= Category.objects.all()
-        context['category_nepal'] = Post.objects.filter(category='1')
-        context['category_automobile'] = Post.objects.filter(category='2')
-        context['category_mobbiles'] = Post.objects.filter(category='3')
-        context['category_computers'] = Post.objects.filter(category='4')
-        context['category_gadget_review'] = Post.objects.filter(category='5')
-        return context 
+        category_posts = Post.objects.all()
+        category_posts = category_posts.filter(
+            category__slug=self.kwargs['slug'])
+        return category_posts
 
-class PostDetail(DetailView):
-    model = Post
-    template_name = 'homepage/post-detail.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories']= Category.objects.all()
+        context['categories'] = Category.objects.all()
         context['category_nepal'] = Post.objects.filter(category='1')
         context['category_automobile'] = Post.objects.filter(category='2')
         context['category_mobbiles'] = Post.objects.filter(category='3')
         context['category_computers'] = Post.objects.filter(category='4')
         context['category_gadget_review'] = Post.objects.filter(category='5')
         context['videos'] = Videos.objects.all()
-        return context 
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'homepage/post-detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['category_nepal'] = Post.objects.filter(category='1')
+        context['category_automobile'] = Post.objects.filter(category='2')
+        context['category_mobbiles'] = Post.objects.filter(category='3')
+        context['category_computers'] = Post.objects.filter(category='4')
+        context['category_gadget_review'] = Post.objects.filter(category='5')
+        context['videos'] = Videos.objects.all()
+        return context
+
 
 def subscribe(request):
     if request.method == "POST":
         form = SubscribeForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request,"Thanks for Subscribing")
-            return redirect('/%20subscribe')
+            messages.success(request, "Thanks for Subscribing")
     else:
         form = SubscribeForm()
-    
-    return render(request, 'homepage/subscribe.html', {'form':form})
+
+    return render(request, 'homepage/index.html', {'form': form})
 
 
 class VideoListView(ListView):
@@ -79,6 +83,5 @@ class VideoListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories']= Category.objects.all()
+        context['categories'] = Category.objects.all()
         return context
-
